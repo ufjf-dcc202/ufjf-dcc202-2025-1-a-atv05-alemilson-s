f570a0447e40f28bf220773c07124ba11c797c3d
Alemilson Silva <alemilson.silva@estudate.ufjf.br>
2025-08-03 13:49:33 -0300

    Correção nas funções de movimentações

--- discos.js
+++ discos.js
@@ -6,26 +6,26 @@ export function getTabuleiro() {
 }
 
 export function selciona(posicao) {
-    if (selecionado == null) {      // Se nenhum disco está selecionado
+    if (selecionado == null) {      
         if (!validaSelecaoOrigem(posicao)) {
             console.log(`Seleção inválida na posição: ${posicao}`);
             return;
         }
         selecionado = posicao;
         console.log(`Disco selecionado na posição: ${posicao}`);
-    } else {            // Se já existe um disco selecionado
+    } else {            
         if (!validaSelecaoDestino(posicao)) {
             console.log(`Seleção inválida na posição: ${posicao}`);
             return;
         }
         if (posicao === selecionado) {
             console.log(`Disco na posição ${posicao} já está selecionado.`);
-            selecionado = null; // Deseleciona o disco
+            selecionado = null; 
             return;
         }
         mover(selecionado, posicao);
         console.log(`Disco movido de ${selecionado} para ${posicao}`);
-        selecionado = null; // Reseta a seleção após o movimento
+        selecionado = null; 
     }
 }
 
@@ -40,18 +40,18 @@ function mover(origem, destino) {
 }
 
 function validaMovimento(origem, destino) {
-    if (origem < 0 || origem >= tabuleiro.length || destino < 0 || destino >= tabuleiro.length) return false; // Verifica se as posições estão dentro do tabuleiro
-    if (tabuleiro[origem] === '' || tabuleiro[destino] !== '') return false; // Verifica se a origem está vazia ou se o destino já tem um disco
+    if (origem < 0 || origem >= tabuleiro.length || destino < 0 || destino >= tabuleiro.length) return false; 
+    if (tabuleiro[origem] === '' || tabuleiro[destino] !== '') return false; 
     return true; 
 }
 
 function validaSelecaoOrigem(posicao) {
-    if (posicao < 0 || posicao >= tabuleiro.length) return false; // Verifica se a posição está dentro do tabuleiro
-    if (tabuleiro[posicao] === '') return false; // Verifica se a posição selecionada tem um disco
+    if (posicao < 0 || posicao >= tabuleiro.length) return false; 
+    if (tabuleiro[posicao] === '') return false; 
     return true;
 }
 function validaSelecaoDestino(posicao) {
-    if (posicao < 0 || posicao >= tabuleiro.length) return false; // Verifica se a posição está dentro do tabuleiro
-    if (tabuleiro[posicao] !== '') return false; // Verifica se a posição selecionada já tem um disco
+    if (posicao < 0 || posicao >= tabuleiro.length) return false;
+    if (tabuleiro[posicao] !== '') return false; 
     return true;
 }
