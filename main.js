24fdc92aa30f4e95f1142a22a7b1ac091d17dc79
Alemilson Silva <alemilson.silva@estudate.ufjf.br>
2025-08-03 13:46:59 -0300

    Criação de função para adicionar tabuleiro

--- main.js
+++ main.js
@@ -2,3 +2,21 @@ import { getTabuleiro, selciona } from "./discos.js";
 
 const eH1 = document.querySelector("h1");
 eH1.textContent = "DCC202 Semana 14";
+
+
+const eTabuleiro = criaTabuleiro();
+document.body.append(eTabuleiro);
+
+function criaTabuleiro() {
+    const eTabuleiro = document.createElement("div");
+    eTabuleiro.classList.add("tabuleiro");
+    return eTabuleiro;
+}
+
+function criaDisco(cor, posicao){
+    const novoDisco = document.createElement("div");
+    novoDisco.classList.add("disco");
+    novoDisco.dataset.cor = cor;
+    novoDisco.dataset.posicao = posicao;
+    return novoDisco;
+}
